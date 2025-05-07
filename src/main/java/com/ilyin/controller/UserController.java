896d0d5f82e4a3650303531a1680944a171e2eeb
Vladislav <stilyin@yandex.ru>
2025-05-07 23:30:50 +0400

    WIP: UserController(get, create), UserRepository(existsByName method), UserService(bean fix)

---
+++ src/main/java/com/ilyin/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.ilyin.controller;
+
+import com.ilyin.domain.Subscription;
+import com.ilyin.domain.User;
+import com.ilyin.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+//TODO exception handler
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    private final UserService userService;
+
+    public record UserDTO(String name, List<Subscription> subscriptions) {
+        public static UserDTO from(User user) {
+            return new UserDTO(user.getName(), user.getSubscriptions());
+        }
+    }
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public UserDTO getUserById(@PathVariable Long id) {
+        return Optional.ofNullable(userService.getUserById(id))
+                .map(UserDTO::from)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public UserDTO createUser(@RequestBody UserDTO userDTO) {
+        User user = new User();
+        user.setName(userDTO.name);
+        user.setSubscriptions(userDTO.subscriptions);
+
+        return UserDTO.from(userService.createUser(user));
+    }
+
+}
--- src/main/java/com/ilyin/service/UserService.java
+++ src/main/java/com/ilyin/service/UserService.java
@@ -3,7 +3,9 @@ package com.ilyin.service;
 import com.ilyin.domain.User;
 import com.ilyin.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
     private final UserRepository userRepository;
 
@@ -13,11 +15,18 @@ public class UserService {
     }
 
     public User createUser(User user) {
+        if (userRepository.existsByName(user.getName())) {
+            throw new IllegalArgumentException("User with this name already exists");
+        }
         return userRepository.save(user);
     }
 
     public User getUserById(Long id) {
         return userRepository.findById(id).orElse(null);
+    }
+
+    public User getUserByName(String name) {
+        return userRepository.findByName(name);
     }
 
     public User saveUser(User user) {
