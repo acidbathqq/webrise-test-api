5096a853086e7bb0d2a44c1ce6078114b8f97a07
Vladislav <stilyin@yandex.ru>
2025-05-07 21:45:48 +0400

    WIP: UserRepository+UserService

---
+++ src/main/java/com/ilyin/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ilyin.repository;
+
+import com.ilyin.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByName(String name);
+}
---
+++ src/main/java/com/ilyin/service/UserService.java
@@ -0,0 +1,31 @@
+package com.ilyin.service;
+
+import com.ilyin.domain.User;
+import com.ilyin.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+}
